2ff1eea274c4fb7cbb8e8b78f9548c7f59bed24e
daryn <darely.quispes@upeu.edu.pe>
2025-09-25 09:27:23 -0500

    splash

--- app/src/main/java/com/example/d4aspostrecitos/activities/SplashActivity.kt
+++ app/src/main/java/com/example/d4aspostrecitos/activities/SplashActivity.kt
@@ -5,10 +5,17 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
+import com.example.d4aspostrecitos.R
 
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, AuthActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 500)
     }
 }
--- app/src/main/java/com/example/d4aspostrecitos/fragment/LoginFragment.kt
+++ app/src/main/java/com/example/d4aspostrecitos/fragment/LoginFragment.kt
@@ -1,4 +1,18 @@
 package com.example.d4aspostrecitos.fragment
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.d4aspostrecitos.R
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.login_fragment, container, false)
+        return view
+    }
 }
